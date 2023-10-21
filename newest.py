904c813af4a04ad9b9603f4a6ea8a68e09b1c2e8
Abylaikhan Bariyev <abylaikhanbariev7@gmail.com>
2023-10-21 10:29:42 +0600

    Data Analysis

--- newest.py
+++ newest.py
@@ -20,6 +20,24 @@ rows = cur.fetchall()
 for row in rows:
     print(row)
 
+# Execute a SELECT query to find the average number of rooms
+cur.execute("SELECT rooms, AVG(rooms) as avg_rooms FROM newhouse GROUP BY rooms ORDER BY rooms;")
+
+# Fetch and process the results
+avg_rooms_rows = cur.fetchall()
+print("\nAverage Number of Rooms:")
+for row in avg_rooms_rows:
+    print(row)
+
+# Execute a SELECT query to find the average price grouped by the number of rooms
+cur.execute("SELECT rooms, AVG(price) as avg_price FROM newhouse GROUP BY rooms ORDER BY rooms;")
+
+# Fetch and process the results
+avg_price_rows = cur.fetchall()
+print("\nAverage Price of Apartments:")
+for row in avg_price_rows:
+    print(row)
+
 # Close cursor and connection
 cur.close()
 conn.close()
