062a329bf3e181de95527d1a557a039c96ace553
Abylaikhan Bariyev <abylaikhanbariev7@gmail.com>
2023-11-25 10:16:42 +0600

    Team Task completed

--- TeamWork/team_task.py
+++ TeamWork/team_task.py
@@ -29,12 +29,15 @@ conn.close()
 
 # 1. Analyze and Clean the Dataset
 # Convert date columns to datetime
-df['DateofHire'] = pd.to_datetime(df['DateofHire'])
-df['DateofTermination'] = pd.to_datetime(df['DateofTermination'])
-df['LastPerformanceReview_Date'] = pd.to_datetime(df['LastPerformanceReview_Date'])
+date_columns = ['DateofHire', 'DateofTermination', 'LastPerformanceReview_Date']
+df[date_columns] = df[date_columns].apply(pd.to_datetime, errors='coerce')
 
 # Handle missing values
-df = df.fillna(df.mean(numeric_only=True))
+# Fill numeric columns with their mean and categorical with the mode
+numeric_cols = df.select_dtypes(include=['number']).columns
+df[numeric_cols] = df[numeric_cols].fillna(df.mean(numeric_only=True))
+categorical_cols = df.select_dtypes(include=['object']).columns
+df[categorical_cols] = df[categorical_cols].fillna(df[categorical_cols].mode().iloc[0])
 
 # 2. Exploratory Data Analysis (EDA)
 print(df.describe())
@@ -56,23 +59,38 @@ sns.scatterplot(x='EngagementSurvey', y='EmpSatisfaction', data=df)
 plt.title('Engagement Survey vs Employee Satisfaction')
 plt.show()
 
+# ... (previous code remains unchanged)
+
 # 5. Regression Analysis
 # Predicting Salary based on EngagementSurvey and EmpSatisfaction
 X = df[['EngagementSurvey', 'EmpSatisfaction']]
 y = df['Salary']
 
+# Splitting the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Creating and fitting the model
 model = LinearRegression()
 model.fit(X_train, y_train)
+
+# Making predictions
 y_pred = model.predict(X_test)
 
-print("Mean Squared Error:", mean_squared_error(y_test, y_pred))
-print("R-squared:", r2_score(y_test, y_pred))
+# Evaluating the model
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+print("Mean Squared Error:", mse)
+print("R-squared:", r2)
 
-# Plotting predictions
-plt.scatter(X_test['EngagementSurvey'], y_test, color='black', label='Actual Salary')
-plt.scatter(X_test['EngagementSurvey'], y_pred, color='blue', label='Predicted Salary')
-plt.xlabel('Engagement Survey')
-plt.ylabel('Salary')
-plt.legend()
+# Create a DataFrame for the actual and predicted values
+results_df = pd.DataFrame({'Actual Salary': y_test, 'Predicted Salary': y_pred})
+# Calculate the correlation matrix including both actual and predicted values
+correlation_matrix = results_df.corr()
+
+# Plotting the heatmap for the correlation matrix
+plt.figure(figsize=(8, 6))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', center=0)
+plt.title('Heatmap of Actual vs Predicted Salary Correlation')
 plt.show()
+
+
